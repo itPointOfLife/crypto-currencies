309c539c77d8b8211733fc6c8224d7522bca5cbc
itPointOfLife <it_original@ukr.net>
2020-05-31 22:01:26 +0300

    Updates

---
+++ precache-manifest.3df4f4998969e18721b0dbb878fb0af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f80923b2c793a85cb5bcf1a23b8b976",
+    "url": "/crypto-currencies/index.html"
+  },
+  {
+    "revision": "0df88bf4059793c4aeb3",
+    "url": "/crypto-currencies/static/css/main.3f976005.chunk.css"
+  },
+  {
+    "revision": "9563f86e68109f0e3d06",
+    "url": "/crypto-currencies/static/js/2.9724da33.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/crypto-currencies/static/js/2.9724da33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df88bf4059793c4aeb3",
+    "url": "/crypto-currencies/static/js/main.e3ffab7f.chunk.js"
+  },
+  {
+    "revision": "98222552a2c7f16f41b3",
+    "url": "/crypto-currencies/static/js/runtime-main.592ac8d4.js"
+  }
+]);
